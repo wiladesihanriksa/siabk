9a3692fc0ed62be27cb65838df9bfc53499a9138
Wilade Sihan Riksa <wilade.sihan23@guru.smk.belajar.id>
2026-03-09 00:22:09 +0700

    fix header

--- app/admin/header_guru_bk.php
+++ app/admin/header_guru_bk.php
@@ -1,5 +1,5 @@
 <?php 
-// 1. Inisialisasi Session & Koneksi
+// 1. Inisialisasi Session & Koneksi (Paling Atas)
 if(!isset($_SESSION)) {
     session_start();
 }
@@ -14,29 +14,20 @@ if(!isset($_SESSION['level']) || $_SESSION['level'] != "guru_bk"){
     exit();
 }
 
-// 3. Ambil Data Settings & Profil (Satu kali proses)
+// 3. Ambil Pengaturan Aplikasi & Profil
 $app_settings = function_exists('getAppSettings') ? getAppSettings($koneksi) : array();
 $color_settings = function_exists('getColorSettings') ? getColorSettings($koneksi) : array();
+
 $id_user = $_SESSION['id'];
-$profil_query = mysqli_query($koneksi, "SELECT * FROM user WHERE user_id='$id_user'");
-$profil = mysqli_fetch_assoc($profil_query);
+$profil_res = mysqli_query($koneksi, "SELECT * FROM user WHERE user_id='$id_user'");
+$profil = mysqli_fetch_assoc($profil_res);
 
-// 4. Logika Gambar Profil & Favicon (Satu variabel untuk semua)
-$baseUrl = function_exists('getSupabaseBaseUrl') ? getSupabaseBaseUrl() : null;
-$user_foto_db = $profil['user_foto'];
-$img_src = (empty($user_foto_db)) ? "../gambar/sistem/user.png" : ($baseUrl ? $baseUrl . 'gambar/user/' . $user_foto_db : '../gambar/user/' . $user_foto_db);
+$app_name = isset($app_settings['app_name']) && $app_settings['app_name'] !== '' ? $app_settings['app_name'] : 'SISBK';
+$institution = isset($app_settings['app_author']) && $app_settings['app_author'] !== '' ? $app_settings['app_author'] : 'Madrasah Aliyah Yasmu';
 
-$app_name = isset($app_settings['app_name']) && $app_settings['app_name'] !== '' ? $app_settings['app_name'] : 'SIABK';
-$institution = isset($app_settings['app_author']) && $app_settings['app_author'] !== '' ? $app_settings['app_author'] : 'Sekolah';
-
+// 4. Logika Favicon
 $fav_from_setting = function_exists('getAppFavicon') ? getAppFavicon($app_settings, '') : '';
 $favicon_path = $fav_from_setting !== '' ? '../' . $fav_from_setting : '../gambar/sistem/logo.png';
-if (strpos($favicon_path, 'http') === false && !file_exists($favicon_path)) {
-    $favicon_path = '../gambar/sistem/login_logo.png';
-}
-
-// Tambahan: Jalankan fungsi akses
-checkGuruBkAccess();
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,19 +35,21 @@ checkGuruBkAccess();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Guru BK - <?php echo htmlspecialchars($app_name . ' ' . $institution); ?></title>
+  <link rel="icon" type="image/png" href="<?php echo $favicon_path; ?>">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
   <link rel="stylesheet" href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../assets/bower_components/Ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="../assets/dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="../assets/dist/css/skins/_all-skins.min.css">
-  <link rel="icon" type="image/png" href="<?php echo $favicon_path; ?>">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <style>
-    .user-panel > .image > img { width: 45px !important; height: 45px !important; object-fit: cover; border-radius: 50%; }
-    .main-header .logo .logo-lg { display: inline-flex; align-items: center; gap: 8px; }
-    .sidebar-mini.sidebar-collapse .user-panel { display: none !important; }
+    /* CSS Fix agar sidebar dan menu bisa diklik/dropdown berjalan */
+    .sidebar-menu { list-style: none; margin: 0; padding: 0; }
+    .main-header .logo { display: flex; align-items: center; justify-content: center; }
+    .user-panel > .image > img { width: 45px; height: 45px; object-fit: cover; }
   </style>
 </head>
 <body class="hold-transition skin-purple sidebar-mini">
@@ -69,7 +62,7 @@ checkGuruBkAccess();
     <a href="guru_bk_dashboard.php" class="logo">
       <span class="logo-mini"><img src="<?php echo $app_logo_path; ?>" style="max-height:22px;"></span>
       <span class="logo-lg">
-        <img src="<?php echo $app_logo_path; ?>" style="max-height:28px;">
+        <img src="<?php echo $app_logo_path; ?>" style="max-height:28px; margin-right:8px;">
         <b><?php echo htmlspecialchars($app_name); ?></b>
       </span>
     </a>
@@ -81,40 +74,35 @@ checkGuruBkAccess();
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <?php 
-          $u_id = $_SESSION['id'];
-          $q_bk = mysqli_query($koneksi, "SELECT guru_bk_id FROM guru_bk WHERE user_id = '$u_id'");
-          $g_data = mysqli_fetch_assoc($q_bk);
-          $g_id = $g_data['guru_bk_id'] ?? 0;
-          
-          $c_kasus = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT COUNT(*) as tot FROM kasus_siswa WHERE guru_bk_id = '$g_id' AND status_kasus = 'Baru' AND sumber_kasus = 'Inisiatif Siswa'"));
-          $c_feed = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT COUNT(*) as tot FROM feedback_siswa f JOIN jurnal_kasus j ON f.jurnal_id = j.jurnal_id JOIN kasus_siswa k ON j.kasus_id = k.kasus_id WHERE k.guru_bk_id = '$g_id' AND (f.is_read = 0 OR f.is_read IS NULL)"));
-          $total_all = $c_kasus['tot'] + $c_feed['tot'];
-          ?>
-          
           <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-bell-o"></i>
-              <?php if($total_all > 0) echo "<span class='label label-warning'>$total_all</span>"; ?>
+              <?php 
+                $u_id = $_SESSION['id'];
+                $q_bk = mysqli_query($koneksi, "SELECT guru_bk_id FROM guru_bk WHERE user_id = '$u_id'");
+                $g_data = mysqli_fetch_assoc($q_bk);
+                $g_id = $g_data['guru_bk_id'] ?? 0;
+                $notif_res = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM kasus_siswa WHERE guru_bk_id = '$g_id' AND status_kasus = 'Baru' AND sumber_kasus = 'Inisiatif Siswa'");
+                $n_count = mysqli_fetch_assoc($notif_res);
+                if($n_count['total'] > 0) echo '<span class="label label-warning">'.$n_count['total'].'</span>';
+              ?>
             </a>
             <ul class="dropdown-menu">
-              <li class="header">Anda memiliki <?php echo $total_all; ?> notifikasi</li>
-              <li>
-                <ul class="menu">
-                  </ul>
-              </li>
+              <li class="header">Anda memiliki <?php echo $n_count['total']; ?> notifikasi baru</li>
+              <li><ul class="menu"></ul></li>
+              <li class="footer"><a href="kasus_siswa.php">Lihat semua</a></li>
             </ul>
           </li>
 
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?php echo $img_src; ?>" class="user-image" alt="User Image">
-              <span class="hidden-xs"><?php echo htmlspecialchars($profil['user_nama']); ?></span>
+              <img src="../gambar/user/<?php echo $profil['user_foto']; ?>" class="user-image" style="object-fit:cover;">
+              <span class="hidden-xs"><?php echo $profil['user_nama']; ?></span>
             </a>
             <ul class="dropdown-menu">
               <li class="user-header">
-                <img src="<?php echo $img_src; ?>" class="img-circle" alt="User Image">
-                <p><?php echo htmlspecialchars($profil['user_nama']); ?><small>Guru BK</small></p>
+                <img src="../gambar/user/<?php echo $profil['user_foto']; ?>" class="img-circle" style="object-fit:cover;">
+                <p><?php echo $profil['user_nama']; ?><small>Guru BK</small></p>
               </li>
               <li class="user-footer">
                 <div class="pull-left"><a href="profil.php" class="btn btn-default btn-flat">Profile</a></div>
@@ -131,17 +119,84 @@ checkGuruBkAccess();
     <section class="sidebar">
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?php echo $img_src; ?>" class="img-circle" style="height:45px; width:45px;">
+          <img src="../gambar/user/<?php echo $profil['user_foto']; ?>" class="img-circle" style="height:45px; width:45px; object-fit:cover;">
         </div>
         <div class="pull-left info">
-          <p><?php echo htmlspecialchars($profil['user_nama']); ?></p>
+          <p><?php echo $profil['user_nama'] ?></p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
-      
+
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">GURU BK NAVIGATION</li>
+        
         <li><a href="guru_bk_dashboard.php"><i class="fa fa-dashboard"></i> <span>DASHBOARD</span></a></li>
-        </ul>
+        
+        <li class="treeview">
+          <a href="#"><i class="fa fa-folder-open"></i> <span>KASUS SISWA</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="kasus_siswa.php"><i class="fa fa-list"></i> Data Kasus Siswa</a></li>
+            <li><a href="kasus_siswa_tambah.php"><i class="fa fa-plus"></i> Tambah Kasus Baru</a></li>
+            <li><a href="laporan_kasus.php"><i class="fa fa-chart-line"></i> Laporan Kasus</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-book"></i> <span>JURNAL KASUS</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="jurnal.php"><i class="fa fa-list"></i> Data Jurnal</a></li>
+            <li><a href="jurnal_tambah.php"><i class="fa fa-plus"></i> Tambah Jurnal</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-home"></i> <span>KUNJUNGAN RUMAH</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="kunjungan_rumah.php"><i class="fa fa-list"></i> Data Kunjungan</a></li>
+            <li><a href="kunjungan_rumah_tambah.php"><i class="fa fa-plus"></i> Tambah Kunjungan</a></li>
+            <li><a href="laporan_kunjungan_rumah.php"><i class="fa fa-chart-bar"></i> Laporan Kunjungan</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-graduation-cap"></i> <span>LAYANAN BK</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="layanan_bk.php"><i class="fa fa-list"></i> Data Layanan BK</a></li>
+            <li><a href="layanan_bk_kalender.php"><i class="fa fa-calendar"></i> Kalender Layanan</a></li>
+            <li><a href="layanan_bk_laporan.php"><i class="fa fa-chart-bar"></i> Laporan Layanan</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-star"></i> <span>POIN SISWA</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="input_prestasi.php"><i class="fa fa-trophy"></i> Data Prestasi</a></li>
+            <li><a href="input_pelanggaran.php"><i class="fa fa-exclamation-triangle"></i> Data Pelanggaran</a></li>
+            <li><a href="laporan.php"><i class="fa fa-chart-pie"></i> Laporan Poin</a></li>
+            <li><a href="cetak_raport.php"><i class="fa fa-print"></i> Cetak Raport</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-user"></i> <span>PROFIL & AKUN</span>
+            <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="profil.php"><i class="fa fa-user"></i> Profil Saya</a></li>
+            <li><a href="gantipassword.php"><i class="fa fa-lock"></i> Ganti Password</a></li>
+            <li><a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
     </section>
   </aside>
